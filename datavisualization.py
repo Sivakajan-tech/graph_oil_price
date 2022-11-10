29403059256bd52348c848889eeafe657cad3d4d
Sivakajan-tech <my.otherwrks@gmail.com>
2022-11-10 21:23:45 +0530

    initial commit

--- datavisualization.py
+++ datavisualization.py
@@ -2,23 +2,33 @@ from h2o_wave import ui, app, main, data
 import pandas as pd
 import sys
 
+# This function is to Handling Files: get the csv file which has the dataset.
+
 
 def fileHandler():
+    # The file name is referenced manually
     fileName = './DataSheet.csv'
     try:
         f = open(fileName, 'r')
     except FileNotFoundError:
+        # Throws error if the csv file is not found
         print(f"File {fileName} not found.  Aborting")
         sys.exit(1)
+        # Throws error if there is any OS errors
     except OSError:
         print(f"OS error occurred trying to open {fileName}")
         sys.exit(1)
     except Exception as err:
+        # Throws error other than the previous errors occur
         print(f"Unexpected error opening {fileName} is", repr(err))
         sys.exit(1)  # or replace this with "raise" ?
     else:
         with f:
             return pd.read_csv(fileName)
+
+# This function is to return the data which is fetched from the csv file
+# It will return a list of tuples;
+# Those tuples have 2 elements; data-> string, price-> float
 
 
 def contentTaker():
@@ -33,17 +43,22 @@ def contentTaker():
     return listOfData[:1100]
 
 
+# Set variable dataSet
 dataSet = contentTaker()
+
+# This function is to convert the all elements in the tuples to string
 
 
 def stringifyContent(intList):
     return [list(map(str, i)) for i in intList]
 
 
+# Indicate that a function is a query handler.
+
 @app('/plot')
+# It control the user inputs after the program run.
 async def controller(q):
-    # Grab a reference to the page at route '/hello'
-    # page = site['/hello']
+    # Grab a reference to the page at route '/plot'
     if not q.client.intialized:
         mainApp(q)
         table_view(q)
@@ -56,13 +71,15 @@ async def controller(q):
     # Finally, save the page.
     await q.page.save()
 
+# This the main app which always show; the program starts,through the all activities
+
 
 def mainApp(q):
     # insted of using site to give a page name, we use app
-
+    # It is used to control attributes of the active page.
     q.page['activePageController'] = ui.meta_card(
         # make the window responsive, and arrnge the every card
-        theme='benext',
+        theme='h2o-dark',
         box='activePageController',
         layouts=[
             ui.layout(
@@ -75,7 +92,7 @@ def mainApp(q):
                     ui.zone('footer'),
                 ]),
         ])
-
+    # The card containing tabs for navigation.
     q.page['navigator'] = ui.tab_card(
         box='navigator',
         items=[
@@ -83,13 +100,14 @@ def mainApp(q):
             ui.tab(name='plot', label="Plot View"),
         ]
     )
+    # The header panel
     q.page['header'] = ui.header_card(
         box='header',  # in top left corner with 2 unit height and width
         subtitle="My first Wave App",
         icon='BarChartVerticalFilterSolid',
         title='''Wave App!! Looking Awesome''',
     )
-
+    # The footer frame
     q.page['footer'] = ui.footer_card(
         box='footer',
         caption='''
@@ -97,11 +115,18 @@ def mainApp(q):
 
         Made with 💛 by Sivakajan Sivaparan!.'''
     )
+    # This helps to view the Table_View when starting the program
     q.client.intialized = True
+
+# A view for table only.
+# Containing all the table related things
 
 
 def table_view(q):
+    # To delete a card named plot_view from a page
+    # It helps to view only the plot_view.
     del q.page['plot_view']
+    # Stringify the content because ui.table -> rows -> table_row expect only strings
     stringDataSet = stringifyContent(dataSet)
     q.page['table_view'] = ui.form_card(
         box='content',
@@ -129,8 +154,12 @@ def table_view(q):
         ],
     )
 
+# A view for graph only.
+# Containing all the graph related things
+
 
 def plot_view(q):
+    # To delete a card named table_view from a page
     del q.page['table_view']
     q.page['plot_view'] = ui.form_card(
         box='content',
